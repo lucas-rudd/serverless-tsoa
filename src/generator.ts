e6b7d8d9480fc74911ce09c25c715892a79bd30e
Lucas Rudd <lucas.rudd@LRUDD-MAC.fourwindsinteractive.com>
2020-01-30 08:26:16 -0700

    updated to use proper-lockfile

--- src/generator.ts
+++ src/generator.ts
@@ -1,17 +1,17 @@
-import { generateSwaggerSpec } from 'tsoa';
-import * as yaml from 'js-yaml';
-import * as fs from 'fs';
-import { SwaggerDefinition } from './models';
-import { DEFAULT_SWAGGER_DEFINITION } from './constants';
-import { moveModels } from './movers';
-import { convertToSwaggerPluginFormat } from './adaptors';
-import * as _ from 'lodash';
-import { green } from 'colors/safe';
-import { parseDirectory } from './utils';
-import { ChildProcess } from 'child_process';
+import { generateSwaggerSpec } from "tsoa";
+import * as yaml from "js-yaml";
+import * as fs from "fs";
+import { SwaggerDefinition } from "./models";
+import { DEFAULT_SWAGGER_DEFINITION } from "./constants";
+import { moveModels } from "./movers";
+import { convertToSwaggerPluginFormat } from "./adaptors";
+import * as _ from "lodash";
+import { green } from "colors/safe";
+import { parseDirectory } from "./utils";
+import { ChildProcess } from "child_process";
 
-const swaggerFile = '/swagger.yaml';
-const modelFile = '/models.yaml';
+const swaggerFile = "/swagger.yaml";
+const modelFile = "/models.yaml";
 
 const thisProcess = (process as NodeJS.EventEmitter) as ChildProcess;
 
@@ -25,13 +25,15 @@ export const swaggerSpecGenerator = async (tsoaConfig: any, outDir: string) => {
       fs.readFileSync(
         parseDirectory(process.cwd()) +
           parseDirectory(tsoaConfig.swagger.outputDirectory) +
-          '/swagger.yaml',
-        'utf8'
+          "/swagger.yaml",
+        "utf8"
       )
     );
     let baseSwaggerDefinition: SwaggerDefinition = DEFAULT_SWAGGER_DEFINITION;
     if (fs.existsSync(baseSwagger)) {
-      baseSwaggerDefinition = yaml.safeLoad(fs.readFileSync(baseSwagger, 'utf8'));
+      baseSwaggerDefinition = yaml.safeLoad(
+        fs.readFileSync(baseSwagger, "utf8")
+      );
     }
     baseSwaggerDefinition = _.merge(baseSwaggerDefinition, swaggerDefinition);
     fs.writeFileSync(baseSwagger, yaml.safeDump(baseSwaggerDefinition));
@@ -41,14 +43,15 @@ export const swaggerSpecGenerator = async (tsoaConfig: any, outDir: string) => {
      */
     await moveModels(swaggerDefinition, baseModel);
     const { components, ...swaggerDefinitionWithoutModels } = swaggerDefinition;
-    const { warnings, convertedSwaggerDefinition } = convertToSwaggerPluginFormat(
-      swaggerDefinitionWithoutModels
-    );
+    const {
+      warnings,
+      convertedSwaggerDefinition
+    } = convertToSwaggerPluginFormat(swaggerDefinitionWithoutModels);
     thisProcess.send({ warnings });
     fs.writeFileSync(
       parseDirectory(process.cwd()) +
         parseDirectory(tsoaConfig.swagger.outputDirectory) +
-        '/swagger.yaml',
+        "/swagger.yaml",
       yaml.safeDump(convertedSwaggerDefinition)
     );
   } catch (error) {
@@ -56,7 +59,7 @@ export const swaggerSpecGenerator = async (tsoaConfig: any, outDir: string) => {
   }
 };
 
-thisProcess.on('message', async ({ tsoaConfig, outDir }) => {
+thisProcess.on("message", async ({ tsoaConfig, outDir }) => {
   try {
     await swaggerSpecGenerator(tsoaConfig, outDir);
     process.exit(0);
