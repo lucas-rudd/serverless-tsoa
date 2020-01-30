2516868f2c97cda29f4348b32b4c6d42ea21bbd8
Lucas Rudd <lucas.rudd@LRUDD-MAC.fourwindsinteractive.com>
2020-01-30 08:32:49 -0700

    actually adding proper-lockfile

--- src/generator.ts
+++ src/generator.ts
@@ -1,4 +1,5 @@
-import { generateSwaggerSpec } from "tsoa";
+import { generateSwaggerSpec, Config } from "tsoa";
+import { lock, unlock, check } from "proper-lockfile";
 import * as yaml from "js-yaml";
 import * as fs from "fs";
 import { SwaggerDefinition } from "./models";
@@ -15,7 +16,10 @@ const modelFile = "/models.yaml";
 
 const thisProcess = (process as NodeJS.EventEmitter) as ChildProcess;
 
-export const swaggerSpecGenerator = async (tsoaConfig: any, outDir: string) => {
+export const swaggerSpecGenerator = async (
+  tsoaConfig: Config,
+  outDir: string
+) => {
   try {
     const baseSwagger = outDir + swaggerFile;
     const baseModel = outDir + modelFile;
@@ -31,17 +35,27 @@ export const swaggerSpecGenerator = async (tsoaConfig: any, outDir: string) => {
     );
     let baseSwaggerDefinition: SwaggerDefinition = DEFAULT_SWAGGER_DEFINITION;
     if (fs.existsSync(baseSwagger)) {
+      await lock(baseSwagger, { retries: 10 });
       baseSwaggerDefinition = yaml.safeLoad(
         fs.readFileSync(baseSwagger, "utf8")
       );
     }
     baseSwaggerDefinition = _.merge(baseSwaggerDefinition, swaggerDefinition);
     fs.writeFileSync(baseSwagger, yaml.safeDump(baseSwaggerDefinition));
+    if (await check(baseSwagger)) {
+      await unlock(baseSwagger);
+    }
     /**
      * move models to model dir
      * then remove from swagger file in the same dir
      */
+    if (fs.existsSync(baseModel)) {
+      await lock(baseModel, { retries: 10 });
+    }
     await moveModels(swaggerDefinition, baseModel);
+    if (await check(baseModel)) {
+      await unlock(baseModel);
+    }
     const { components, ...swaggerDefinitionWithoutModels } = swaggerDefinition;
     const {
       warnings,
